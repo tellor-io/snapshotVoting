3c551aa5909735075eb28dc4b397af4039db89a6
Quentin <thealmightyquinn@hotmail.com>
2022-05-05 10:40:12 +0200

    comment

--- hardhat.config.js
+++ hardhat.config.js
@@ -26,20 +26,20 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 module.exports = {
   solidity: "0.8.4",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    rinkeby: {
-      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [`${process.env.PRIVATE_KEY}`],
-      gas: 10000000 ,
-      gasPrice: 40000000000
- },
-    forking: {
-      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    },
+//     ropsten: {
+//       url: process.env.ROPSTEN_URL || "",
+//       accounts:
+//         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+//     },
+//     rinkeby: {
+//       url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+//       accounts: [`${process.env.PRIVATE_KEY}`],
+//       gas: 10000000 ,
+//       gasPrice: 40000000000
+//  },
+//     forking: {
+//       url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+//     },
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
